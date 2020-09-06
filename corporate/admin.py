b77a170878db686f77e62781f7b1345dd4491927
alishalawani <alishalawani@gmail.com>
2020-09-06 15:21:10 -0500

    basic setups

--- corporate/admin.py
+++ corporate/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from . import models
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['id', 'email', 'name', 'is_superuser', 'last_login']
+  
+    fieldsets = (
+      (None, {'fields': ('email', 'password')}),
+      ('Personal Info', {'fields': ('name',)}),
+      ('Permissions',
+          {
+              'fields': (
+                  'is_active',
+                  'is_staff',
+                  'is_superuser',
+              )
+          }
+      ),
+      ('Dates', {'fields': ('last_login',)}),
+    )
+  
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2')
+        }),
+    )
+
+# register the model and tell Django to use the above UserAdmin
+# class to format the pages:
+admin.site.register(models.User, UserAdmin)
--- corporate/views.py
+++ corporate/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User, Review, Company
+from .serializers import UserSerializer, CompanySerializer, ReviewSerializer
 
 # Create your views here.
+class UserView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CompanyView(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class ReviewView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
