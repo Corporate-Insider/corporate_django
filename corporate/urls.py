66d0937f066da0edf08c057c33981060e3df6b4e
alishalawani <alishalawani@gmail.com>
2020-09-06 15:18:13 -0500

    set up urls

---
+++ corporate/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+import os
+
+api_key = os.environ['API_KEY']
+
+urlpatterns = [
+    path(f'users/key={api_key}', views.UserView.as_view(), name='user_list'),
+    path('companies/', views.CompanyView.as_view(), name='company_list'),
+    path('reviews/', views.ReviewView.as_view(), name='review_list'),
+    path(f'users/<int:pk>/key={api_key}', views.UserDetail.as_view(), name='user_detail'),
+    path('companies/<int:pk>', views.CompanyDetail.as_view(), name='company_detail'),
+    path('reviews/<int:pk>', views.ReviewDetail.as_view(), name='review_detail'),
+]
