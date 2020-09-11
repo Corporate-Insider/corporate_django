01ba59d3c229bc36d54e98ccdaf4ad53661697e2
alishalawani <alishalawani@gmail.com>
2020-09-10 19:54:22 -0500

    modified model and serializers files

--- corporate/models.py
+++ corporate/models.py
@@ -2,53 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.conf import settings
 from accounts.models import User
-
-# Create your models here.
-
-# class UserManager(BaseUserManager):
-#     """Manager for user profiles"""
-
-  
-#     def create_user(self, email, name, password=None, **extra_fields):
-#         """Create a new user profile"""
-#         # Add a custom validation error
-#         if not email:
-#             raise ValueError('User must have an email address')
-
-       
-#         user = self.model(email=self.normalize_email(email), name=name, **extra_fields)
-
-#         # Use the set_password method to hash the password
-#         user.set_password(password)
-#         user.save()
-
-#         return user
-
-#     def create_superuser(self, email, name, password):
-#         """Create and save a new superuser with given details"""
-#         user = self.create_user(email, name, password)
-#         user.is_superuser = True
-#         user.is_staff = True
-#         user.save()
-
-#         return user
-
-#         # Inherit from AbstractBaseUser and PermissionsMixin:
-# class User(AbstractBaseUser, PermissionsMixin):
-#     """Database model for users"""
-#     email = models.EmailField(max_length=255, unique=True)
-#     name = models.CharField(max_length=255)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-
-#     objects = UserManager()
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['name']
-
-#     def __str__(self):
-#         """Return string representation of the user"""
-#         return self.email
         
 class Company(models.Model):
     name = models.CharField(max_length=50)
--- corporate/serializers.py
+++ corporate/serializers.py
@@ -3,8 +3,6 @@ from .models import Company, Review
 from accounts.models import User
 
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
-    review_url = serializers.ModelSerializer.serializer_url_field(view_name='review_detail')
-
     company = serializers.HyperlinkedRelatedField(
         view_name='company_detail',
         read_only=True
@@ -19,21 +17,11 @@ class ReviewSerializer(serializers.HyperlinkedModelSerializer):
     )
     class Meta:
         model = Review
-        fields = ('id', 'review','rating','rated','review_url','user_id','company', 'company_id')
+        fields = ('id', 'review','rating','rated','user_id','company', 'company_id')
         
-
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     reviews = ReviewSerializer(many=True, read_only=True)
-  
-#     class Meta:
-#         model = User
-#         fields = ('id', 'name','email', 'password', 'reviews')
-
-
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
-    url = serializers.ModelSerializer.serializer_url_field(view_name='company_detail')
 
     class Meta:
         model = Company
-        fields =('id', 'name','domain', 'logo','reviews','url')
+        fields =('id', 'name','domain', 'logo','reviews')
