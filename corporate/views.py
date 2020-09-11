456696dd3dac3583f44fd93540646eaccea4a083
alishalawani <alishalawani@gmail.com>
2020-09-10 19:53:47 -0500

    modified the views of the corporate app

--- corporate/views.py
+++ corporate/views.py
@@ -1,23 +1,26 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
 from .models import User, Review, Company
 from accounts.serializers import UserSerializer
 from .serializers import CompanySerializer, ReviewSerializer
 
 # Create your views here.
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
+@permission_classes((AllowAny, ))
 class CompanyView(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+@permission_classes((AllowAny, ))
 class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-
+    
+@permission_classes((AllowAny, ))
 class ReviewView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+@permission_classes((AllowAny, ))
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
