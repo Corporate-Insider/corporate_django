84d31149402ac392c077e38e38826e40601c4b2a
alishalawani <alishalawani@gmail.com>
2020-09-10 19:52:52 -0500

    set urls for the accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import current_user, UserCreate, UserView
+from .views import current_user, UserCreate, UserView, UserDetail
 import os
 api_key = os.environ['API_KEY']
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('current_user/', current_user),
     path('create_user/', UserCreate.as_view()),
     path(f'users/{api_key}', UserView.as_view()),
+    path(f'users/<int:pk>/{api_key}', UserDetail.as_view()),
 ]
