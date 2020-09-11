915e466c570f9c68249efb99cb7744b654fe9a46
alishalawani <alishalawani@gmail.com>
2020-09-10 19:53:23 -0500

    added api key to some routes in the corporate app

--- corporate/urls.py
+++ corporate/urls.py
@@ -7,11 +7,9 @@ from rest_framework_jwt.views import obtain_jwt_token
 api_key = os.environ['API_KEY']
 
 urlpatterns = [
-    # path(f'users/key={api_key}', views.UserView.as_view(), name='user_list'),
     path('companies/', views.CompanyView.as_view(), name='company_list'),
     path('reviews/', views.ReviewView.as_view(), name='review_list'),
-    # path(f'users/<int:pk>/key={api_key}', views.UserDetail.as_view(), name='user_detail'),
-    path('companies/<int:pk>', views.CompanyDetail.as_view(), name='company_detail'),
-    path('reviews/<int:pk>', views.ReviewDetail.as_view(), name='review_detail'),
+    path(f'companies/<int:pk>/{api_key}', views.CompanyDetail.as_view(), name='company_detail'),
+    path(f'reviews/<int:pk>/{api_key}', views.ReviewDetail.as_view(), name='review_detail'),
     path('token-auth/', obtain_jwt_token)
 ]
