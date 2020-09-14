d7525586de4f6cbaf29202a3e600fff36ab1dfd0
Ubuntu <ubuntu@ip-172-31-36-108.us-east-2.compute.internal>
2020-09-14 02:30:21 +0000

    made some changes within the nano shell

--- corporate_django/wsgi.py
+++ corporate_django/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/corporate_django')
 
 from django.core.wsgi import get_wsgi_application
-
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/corporate_django/egg_cache")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'corporate_django.settings')
 
 application = get_wsgi_application()
