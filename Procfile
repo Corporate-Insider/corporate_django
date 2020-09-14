96b369d0bbccccaaed834ec7c18b38363e247aa8
alishalawani <alishalawani@gmail.com>
2020-09-14 13:38:58 -0500

    ran collectstatic

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn corporate_django.wsgi
--- corporate_django/settings.py
+++ corporate_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 import environ
 env = environ.Env(
     # set casting, default value
