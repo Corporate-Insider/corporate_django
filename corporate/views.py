3359e7f1eb1473b55abc94a59d7db1f52ba6c5ce
alishalawani <alishalawani@gmail.com>
2020-09-13 21:28:52 -0500

    imported environ in settings file

--- corporate/views.py
+++ corporate/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes
-from .models import User, Review, Company
+from .models import User, Review, Company, Rating
 from accounts.serializers import UserSerializer
-from .serializers import CompanySerializer, ReviewSerializer
+from .serializers import CompanySerializer, ReviewSerializer, RatingSerializer
 
 # Create your views here.
 @permission_classes((AllowAny, ))
@@ -24,3 +24,12 @@ class ReviewView(generics.ListCreateAPIView):
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+@permission_classes((AllowAny, ))
+class RatingView(generics.ListCreateAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+@permission_classes((AllowAny, ))
+class RatingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
