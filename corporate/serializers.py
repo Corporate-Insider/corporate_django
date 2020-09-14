d7764673bd388e3479aa3db82535dea321972178
alishalawani <alishalawani@gmail.com>
2020-09-13 21:27:54 -0500

    created serializer for the newly created model and editted other serializers

--- corporate/serializers.py
+++ corporate/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Company, Review
+from .models import Company, Review, Rating
 from accounts.models import User
 
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,11 +17,28 @@ class ReviewSerializer(serializers.HyperlinkedModelSerializer):
     )
     class Meta:
         model = Review
-        fields = ('id', 'review','rating','rated','user_id','company', 'company_id')
+        fields = ('id', 'review','user_id','company', 'company_id')
         
+
+class RatingSerializer(serializers.HyperlinkedModelSerializer):
+    company = serializers.HyperlinkedRelatedField(
+        view_name='company_detail',
+        read_only=True
+    )
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset = User.objects.all(),
+        source = 'user'
+    )
+    company_id = serializers.PrimaryKeyRelatedField(
+        queryset = Company.objects.all(),
+        source = 'company'
+    )
+    class Meta:
+        model = Rating
+        fields = ('id', 'rating','user_id','company', 'company_id')
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
-
+    ratings = RatingSerializer(many=True, read_only=True)
     class Meta:
         model = Company
-        fields =('id', 'name','domain', 'logo','reviews')
+        fields =('id', 'name','domain', 'logo','ratings','reviews')
